d0a81863201b96245cd604653b074c306dbb29f7
thanhlamcode <lamdoan1122334455@gmail.com>
2025-05-03 14:26:13 +0000

    chore: One-To-One, Self-referencing

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping AS ORM;
 
 #[ApiResource]
@@ -20,5 +21,13 @@ class User
     #[ORM\Column(type: 'string', length: 255)]
     private string $password;
 
+    // Quan hệ: Nhiều user thuộc về 1 manager
+    #[ORM\ManyToOne(targetEntity: self::class, inversedBy: 'subordinates')]
+    #[ORM\JoinColumn(name: 'manager_id', referencedColumnName: 'id', nullable: true)]
+    private ?User $manager = null;
+
+    // Quan hệ: 1 manager quản lý nhiều user
+    #[ORM\OneToMany(targetEntity: self::class, mappedBy: 'manager')]
+    private Collection $subordinates;
 
 }
